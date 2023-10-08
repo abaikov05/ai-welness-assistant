c523a79ac67c0dd83bae40e4d677c5b56aae0a3c
abaikov05 <abaikov05@gmail.com>
2023-10-08 11:45:03 +0300

    added temtlate + ststic file

--- web/app/views.py
+++ web/app/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(response):
-    return render(response, "app/home.html")
+    return render(response, "app/home.html", {})
+def chat(response):
+    return render(response, "app/chat.html", {})
